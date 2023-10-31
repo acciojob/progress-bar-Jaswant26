857d4d8fb8b86a918cbef2a24855c073f32be8aa
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-31 14:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+ const circles = document.querySelectorAll('.circle');
+    const nextButton = document.getElementById('next');
+    const prevButton = document.getElementById('prev');
+    let currentIndex = 0;
+
+    // Add event listener to the "Next" button
+    nextButton.addEventListener('click', () => {
+      if (currentIndex < circles.length - 1) {
+        circles[currentIndex].classList.remove('active');
+        currentIndex++;
+        circles[currentIndex].classList.add('active');
+        prevButton.disabled = false;
+        if (currentIndex === circles.length - 1) {
+          nextButton.disabled = true;
+        }
+      }
+    });
+
+    // Add event listener to the "Previous" button
+    prevButton.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        circles[currentIndex].classList.remove('active');
+        currentIndex--;
+        circles[currentIndex].classList.add('active');
+        nextButton.disabled = false;
+        if (currentIndex === 0) {
+          prevButton.disabled = true;
+        }
+      }
+    });
